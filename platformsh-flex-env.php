56b4e83a8d5a4a3fb86f4e6201a9d00f3af91ea4
Larry Garfield <larry@platform.sh>
2018-03-06 11:07:55 -0600

    Move the DB relationship logic to its own function for easier maintenance.

--- platformsh-flex-env.php
+++ platformsh-flex-env.php
@@ -30,10 +30,13 @@ function mapPlatformShEnvironment() : void
         $_SERVER['APP_ENV'] = 'prod';
     }
 
-    if (isset($_SERVER['DATABASE_URL'])) {
-        return;
+    if (!isset($_SERVER['DATABASE_URL'])) {
+        mapPlatformShDatabase();
     }
+}
 
+function mapPlatformShDatabase() : void
+{
     // Set the DATABASE_URL for Doctrine, if necessary.
     # "mysql://root@127.0.0.1:3306/symfony?charset=utf8mb4&serverVersion=5.7";
     if (isset($_SERVER['PLATFORM_RELATIONSHIPS'])) {
@@ -85,4 +88,5 @@ function mapPlatformShEnvironment() : void
     );
 
     $_SERVER['DATABASE_URL'] = $dbUrl;
+
 }
