773cacc1f6e8a698460297d8a3e2294721685d3c
Larry Garfield <larry@platform.sh>
2018-03-06 11:30:01 -0600

    Add PHPUnit for testing.

---
+++ tests/FlexBridgeTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Platformsh\FlexBridge\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FlexBridgeTest extends TestCase
+{
+
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+
+}
