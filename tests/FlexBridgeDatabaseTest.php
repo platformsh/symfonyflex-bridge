f327f5632942f6cde04208816c7ca2f837b8ad7b
Larry Garfield <larry@platform.sh>
2019-10-03 13:46:16 -0500

    Overhaul DB version derivation logic now that we support a broader variety of databases and versions.

--- tests/FlexBridgeDatabaseTest.php
+++ tests/FlexBridgeDatabaseTest.php
@@ -6,6 +6,9 @@ namespace Platformsh\FlexBridge\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Platformsh\FlexBridge\mapPlatformShEnvironment;
+use const Platformsh\FlexBridge\DEFAULT_MARIADB_VERSION;
+use const Platformsh\FlexBridge\DEFAULT_MYSQL_VERSION;
+use const Platformsh\FlexBridge\DEFAULT_POSTGRESQL_VERSION;
 
 class FlexBridgeDatabaseTest extends TestCase
 {
@@ -81,18 +84,6 @@ class FlexBridgeDatabaseTest extends TestCase
         $this->assertArrayNotHasKey('DATABASE_URL', $_SERVER);
     }
 
-    public function testDatabaseRelationshipSet() : void
-    {
-        putenv('PLATFORM_APPLICATION_NAME=test');
-        putenv('PLATFORM_ENVIRONMENT=test');
-        putenv('PLATFORM_PROJECT_ENTROPY=test');
-        putenv('PLATFORM_SMTP_HOST=1.2.3.4');
-        putenv(sprintf('PLATFORM_RELATIONSHIPS=%s', base64_encode(json_encode($this->relationships))));
-
-        mapPlatformShEnvironment();
-        $this->assertEquals('mysql://user:@database.internal:3306/main?charset=utf8mb4&serverVersion=mariadb-10.2.12', $_SERVER['DATABASE_URL']);
-    }
-
     public function testDatabaseRelationshipOnFoundation1() : void
     {
         putenv('PLATFORM_APPLICATION_NAME=test');
@@ -112,4 +103,99 @@ class FlexBridgeDatabaseTest extends TestCase
         $this->assertEquals('mysql://user:@database.internal:3306/main?charset=utf8mb4&serverVersion=mariadb-10.2.12', $_SERVER['DATABASE_URL']);
     }
 
+    /**
+     * @dataProvider databaseVersionsProvider
+     */
+    public function testDatabaseRelationshipFormatters(string $type, string $scheme, string $expected) : void
+    {
+        putenv('PLATFORM_APPLICATION_NAME=test');
+        putenv('PLATFORM_ENVIRONMENT=test');
+        putenv('PLATFORM_PROJECT_ENTROPY=test');
+        putenv('PLATFORM_SMTP_HOST=1.2.3.4');
+
+        $rels = $this->relationships;
+
+        $rels['database'][0]['type'] = $type;
+        $rels['database'][0]['scheme'] = $scheme;
+
+        putenv(sprintf('PLATFORM_RELATIONSHIPS=%s', base64_encode(json_encode($rels))));
+
+        mapPlatformShEnvironment();
+
+        $this->assertEquals($expected, $_SERVER['DATABASE_URL']);
+    }
+
+    /**
+     * @dataProvider databaseVersionsProvider
+     */
+    public function testDatabaseRelationshipFormattersFoundationV1(string $type, string $scheme, string $expected) : void
+    {
+        putenv('PLATFORM_APPLICATION_NAME=test');
+        putenv('PLATFORM_ENVIRONMENT=test');
+        putenv('PLATFORM_PROJECT_ENTROPY=test');
+        putenv('PLATFORM_SMTP_HOST=1.2.3.4');
+
+        $rels = $this->relationships;
+
+        $rels['database'][0]['scheme'] = $scheme;
+
+        // On Foundation 1, there is no `type` property.  Therefore we do not know what the DB version
+        // should be.  So we fall back to the default guesses and hope. If someone wants to use a different
+        // version, they should move to Foundation 3.
+        unset($rels['database'][0]['type']);
+        switch ($scheme) {
+            case 'mysql':
+                $default = 'mariadb-' . DEFAULT_MARIADB_VERSION . '.12';
+                break;
+            case 'pgsql':
+                $default = DEFAULT_POSTGRESQL_VERSION;
+        }
+        $expected = preg_replace('/serverVersion=.+$/', 'serverVersion=' . $default, $expected);
+
+        putenv(sprintf('PLATFORM_RELATIONSHIPS=%s', base64_encode(json_encode($rels))));
+
+        mapPlatformShEnvironment();
+
+        $this->assertEquals($expected, $_SERVER['DATABASE_URL']);
+    }
+
+
+    public function databaseVersionsProvider() : iterable
+    {
+        yield 'postgresql 9.6' => [
+            'type' => 'postgresql:9.6',
+            'scheme' => 'pgsql',
+            'expected' => 'pgsql://user:@database.internal:3306/main?serverVersion=9.6',
+        ];
+        yield 'postgresql 10' => [
+            'type' => 'postgresql:10',
+            'scheme' => 'pgsql',
+            'expected' => 'pgsql://user:@database.internal:3306/main?serverVersion=10',
+        ];
+
+        // This is the oddball that doesn't have a .0, because reasons.
+        yield 'mariadb 10.2' => [
+            'type' => 'mariadb:10.2',
+            'scheme' => 'mysql',
+            'expected' => 'mysql://user:@database.internal:3306/main?charset=utf8mb4&serverVersion=mariadb-10.2.12',
+        ];
+
+        yield 'mariadb 10.4' => [
+            'type' => 'mariadb:10.4',
+            'scheme' => 'mysql',
+            'expected' => 'mysql://user:@database.internal:3306/main?charset=utf8mb4&serverVersion=mariadb-10.4.0',
+        ];
+
+        yield 'mariadb 10.2 aliased' => [
+            'type' => 'mysql:10.2',
+            'scheme' => 'mysql',
+            'expected' => 'mysql://user:@database.internal:3306/main?charset=utf8mb4&serverVersion=mariadb-10.2.12',
+        ];
+
+        yield 'mysql 8' => [
+            'type' => 'oracle-mysql:8.0',
+            'scheme' => 'mysql',
+            'expected' => 'mysql://user:@database.internal:3306/main?charset=utf8mb4&serverVersion=8.0',
+        ];
+    }
 }
